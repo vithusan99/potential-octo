563e63a2a4684450469b3aeec21c315b74b63e95
vithusan99 <65951485+vithusan99@users.noreply.github.com>
2020-08-08 14:27:53 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -4,33 +4,33 @@ import {Button, Box, makeStyles, AppBar,Toolbar, Grid } from '@material-ui/core'
 import CollectionsBookmarkOutlinedIcon from '@material-ui/icons/Collections'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Body from './Components/Body'
-import HI from './Components/HI'
-import Acecreed from './Components/Acecreed'
-import State from './Components/State'
-import First from './exercise/First'
-import Condition1 from './Components/Condition1'
-import SmallOne from './Components/SmallOne'
-import LoginLogout from './Components/LoginLogout';
-import NameList from './Components/Namelist';
-import Stylesheet from './Components/Stylesheet'
-import Forms from './Components/Forms'
-import RefsDemo from './Components/Column';
-import Sampleportal from './Components/Portsample';
-import WithIncrement from './Components/Increment';
-import DemoTable from './Components/Table';
-// import RepositoryList from './Repository';
-import Main from './Components/Main';
-import Contact from './Components/Contact';
-import HookCounter from './Components/Increment';
-import HookPro from './Components/HookPrac';
-// import Signup from './Components/Signup';
-import Parent from './Components/Parent';
-import Newone from './Components/Newone'
-import EventBind from './Components/Ex';
-import Navbar from './Components/Navbar';
+// import Body from './Components/Body'
+// import HI from './Components/HI'
+// // import Acecreed from './Components/Acecreed'
+// import State from './Components/State'
+// import First from './exercise/First'
+// import Condition1 from './Components/Condition1'
+// import SmallOne from './Components/SmallOne'
+// import LoginLogout from './Components/LoginLogout';
+// import NameList from './Components/Namelist';
+// import Stylesheet from './Components/Stylesheet'
+// import Forms from './Components/Forms'
+// import RefsDemo from './Components/Column';
+// import Sampleportal from './Components/Portsample';
+// import WithIncrement from './Components/Increment';
+// import DemoTable from './Components/Table';
+// // import RepositoryList from './Repository';
+// import Main from './Components/Main';
+// import Contact from './Components/Contact';
+// import HookCounter from './Components/Increment';
+// import HookPro from './Components/HookPrac';
+// // import Signup from './Components/Signup';
+// import Parent from './Components/Parent';
+// import Newone from './Components/Newone'
+// import EventBind from './Components/Ex';
+// import Navbar from './Components/Navbar';
 import Success from './Components/Success';
-import ParentComponent from './Components/Example1';
+// import ParentComponent from './Components/Example1';
 
 import Signin from "./Components/Signin";
 import Mate from "./Components/Mate";
@@ -48,7 +48,7 @@ import Booklist from './Components/Booklist';
 
 const style={
 appbar:{
-    backgroundColor:"black",
+    backgroundColor:"#33ccff",
     color:"white",
     fontFamily:"fontFamily",
     padding:8
@@ -60,6 +60,19 @@ nav:{
 book:{
     fontSize:30,
 },
+// but:{
+//   color:"red",
+//   padding:5,
+//   top:20
+
+// },
+top:{
+  margin:30,
+  color:"red",
+  padding:5,
+  top:20
+
+}
 };
 
 
@@ -109,34 +122,57 @@ return(
     <AppBar position="fixed">
     <Grid container style={style.appbar}>
         <Grid container xs={4}  > 
-    <Grid xs={4} style={style.book}><CollectionsBookmarkOutlinedIcon/>Book Shop</Grid>
-    <Grid xs={2}>
+    <Grid xs={4} style={style.book}><img src="src/Components/IMG-20200725-WA0012 (1).jpg"/></Grid>
+    {/* <Grid xs={2}>
     <Button style={style.nav} href="/home"><strong>HOME</strong></Button>
-    </Grid>
+    </Grid> */}
     
-    { currentUser && (<Grid container  xs={6} >
+    {/* { currentUser && (<Grid container  xs={6} >
       <Grid xs={6} ><Button style={style.nav} href="/addBook"><strong>ADD BOOK</strong></Button></Grid>
                 <Grid xs={6}><Button style={style.nav} href="/booklist"><strong>BOOK LIST</strong></Button></Grid>
-                </Grid>)}
+                </Grid>)} */}
     </Grid>
     <Grid xs={6}/>
    
-    {currentUser ? (
+    {/* {currentUser ? (
               <Grid container xs={2} >
     <Grid xs={6}><Button style={style.nav} href="/profile"> <strong><AccountCircle style={{fontSize:27}}/>{currentUser.username}</strong></Button></Grid>
                <Grid xs={6} ><Button style={style.nav} href="./login" onClick={this.logOut()}><strong>LOGOUT</strong></Button></Grid>
-               </Grid> 
-    ):( <Grid container xs={2}>
+               </Grid>  */}
+     <Grid container xs={2}>
           <Grid xs={6} ><Button style={style.nav} href="/login"> <strong>LOGIN</strong></Button></Grid>
         <Grid xs={6} ><Button style={style.nav} href="./signin" ><strong>SIGNUP</strong></Button></Grid>
         </Grid>
-          )}
+          
     </Grid>
 
 </AppBar>
 <div>
+  <div>
+  <Grid container style={style.top}>
+          <Grid container xs={8}>
+          <Grid xs={2}>
+    <Button style={style.top} href="/home"><strong>HOME</strong></Button>
+    </Grid>
+    <Grid xs={2}>
+    <Button style={style.top} href="/home"><strong>Our Product</strong></Button>
+    </Grid>
+    <Grid xs={2}>
+    <Button style={style.top} href="/home"><strong>Products Prices</strong></Button>
+    </Grid>
+    <Grid xs={2}>
+    <Button style={style.top} href="/home"><strong>Contact Us</strong></Button>
+    </Grid>
+  </Grid>
+  <Grid xs={4} container >
+    <Grid xs={2}/>
+    <Grid xs={2}><Button style={style.top} href="/home"><strong>Search</strong></Button></Grid>
+  </Grid>
+  </Grid>
+  </div>
+
             <Switch>
-              <Route exact path={["/", "/home"]} component={Mate} />
+              <Route exact path={[ "/home"]} component={Mate} />
               <Route exact path={"/login"} component={Login} />
               <Route exact path="/signin" component={Signin} />
               <Route exact path="/profile" component={Profile} />
--- Footer.js
+++ Footer.js
@@ -4,25 +4,43 @@ import CollectionsBookmarkOutlinedIcon from '@material-ui/icons/Collections'
 
 const new1Style=makeStyles({
 botbar:{
-    backgroundColor:"black",
+  position:"fixed",
+    backgroundColor:"#33ccff",
     color:"white",
     width:"100%",
-    top:"auto",
+    height:300,
+    fontSize:20,
     bottom:0,
-    height:30,
-    paddingTop:5
-}  
+    left:0
+}
+// },
+// hover:botbar:{
+//    color:"red"
+// }
 } );           
 
 
 function Footer(){
 
   const classes= new1Style();
-  let fullYear=new Date(). getFullYear();
+  // let fullYear=new Date(). getFullYear();
 return(
 
-        <Box  textAlign="center">
-        <AppBar position={"fixed"} className={classes.botbar}  >{fullYear}-{fullYear+1}, ALL RIGHT RESERVED BY ANITH</AppBar>
+        // <Box  textAlign="center">
+        // <AppBar position={"fixed"} className={classes.botbar}  >{fullYear}-{fullYear+1}, ALL RIGHT RESERVED BY ANITH</AppBar>
+        // </Box>
+        <Box className={classes.botbar}  >
+         <Grid container>
+           <Grid xs={3}  >
+          <p>Contact Us</p>
+         <p> Social media links:</p>
+         <p> E-mail Address:</p>
+          
+          </Grid>
+          <Grid xs={3}/>
+          <Grid xs={3}><p>About Us :</p></Grid>
+          </Grid>
+          
         </Box>
 
 )
